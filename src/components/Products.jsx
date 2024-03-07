4531fb1680edb291c81675a3550b10d6fb2cd4a3
AntonioEspin <antoesdev91@gmail.com>
2024-03-07 07:02:58 -0500

    add removeIcon and conditional

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,27 +1,45 @@
 /* eslint-disable react/prop-types */
-import {AddToCartIcon} from '../components/Icons'
+import {AddToCartIcon, RemoveFromCartIcon} from '../components/Icons'
 import { useCart } from '../hooks/useCart'
 import './Products.css'
 
 export function Products ({products}) {
-  const {addToCart} = useCart()
+  const {addToCart,removeFromCart, cart} = useCart()
+  
+  const checkProductInCart = (product) => {
+    return cart.some(item => item.id === product.id)
+  }
+
+  // const handleClickButtonCart = (product, isProductInCart) => {
+  //   isProductInCart
+  //     ? removeFromCart(product)
+  //     : addToCart(product)
+  // }
+
   return (
     <main className='products'>
       <ul>
-        {products.map(product => (
-          <li key={product.id}>
-            <img src={product.image} alt={product.description} />
-            <div>
-              <strong>{product.title}</strong> - ${product.price}
-            </div>
+        {products.map(product => {
+          const isProductInCart = checkProductInCart(product)
+          return (
+            <li key={product.id}>
+              <img src={product.image} alt={product.description} />
+              <div>
+                <strong>{product.title}</strong> - ${product.price}
+              </div>
 
-            <div>
-              <button onClick={()=> addToCart(product)}>
-                <AddToCartIcon/>
-              </button>
-            </div>
-          </li>
-        ))}
+              <div>
+                <button style={{backgroundColor: isProductInCart ? 'red': '#09f'}} onClick={()=> isProductInCart ? removeFromCart(product) : addToCart(product)}>
+                  {
+                    isProductInCart
+                      ? <RemoveFromCartIcon/>
+                      : <AddToCartIcon/>
+                  }
+                </button>
+              </div>
+            </li>
+          )
+        })}
       </ul>
     </main>
   )
--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -27,6 +27,10 @@ export function CartProvider ({children}) {
     ]))
   }
 
+  const removeFromCart = product => {
+    setCart(prevState => prevState.filter(item => item.id !== product.id))
+  }
+
   const clearCart = () => {
     setCart([])
   }
@@ -34,6 +38,7 @@ export function CartProvider ({children}) {
     <CartContext.Provider value={{
       cart,
       addToCart,
+      removeFromCart,
       clearCart
     }}>
       {children}
